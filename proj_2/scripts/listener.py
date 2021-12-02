978c0d5f7ed12ac9cb2680fca159eaade58e0fd7
jpittma1 <91224170+jpittma1@users.noreply.github.com>
2021-12-02 10:46:26 -0500

    Create listener.py

---
+++ proj_2/scripts/listener.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+##Project 2 Script Subscriber
+
+import rospy
+from std_msgs.msg import Float64
+
+def callback(data):
+    rospy.loginfo("I heard %s", data.data)
+
+def listener():
+
+    rospy.init_node('listener', anonymous=True)
+    # pub_J1 = rospy.Publisher('/proj_2/PositionJointInterface_J1_controller/command', Float64, queue_size=10) 
+    rospy.Subscriber("/proj_2/PositionJointInterface_J1_controller/command", Float64, callback)
+    rospy.Subscriber("/proj_2/PositionJointInterface_J2_controller/command", Float64, callback)
+    rospy.Subscriber("/proj_2/PositionJointInterface_J3_controller/command", Float64, callback)
+    rospy.Subscriber("/proj_2/PositionJointInterface_J4_controller/command", Float64, callback)
+    rospy.Subscriber("/proj_2/PositionJointInterface_J6_controller/command", Float64, callback)
+    rospy.Subscriber("/proj_2/PositionJointInterface_J7_controller/command", Float64, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
