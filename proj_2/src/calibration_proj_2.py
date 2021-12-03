98aed63d4238bda03d3832ee3f002d840d983076
jpittma1 <91224170+jpittma1@users.noreply.github.com>
2021-12-03 15:00:19 -0500

    Update calibration_proj_2.py

--- proj_2/src/calibration_proj_2.py
+++ proj_2/src/calibration_proj_2.py
@@ -34,88 +34,49 @@ theta6=symbols('theta6')
 theta7=symbols('theta7')
 
 #--Measurements from Figure 2 (in meters)--
-d1=0.36 #360mm
-d3=0.42
-d5=(201+198.5)/1000 
-d7=0.1055 
+d02=0.36 #joint 0 to 2
+d01=d02/2 #180mm #joint 0 to 1
+d12=d02/2 #joint 1 to 2
+d24=0.42 #joint 2 to 4
+d23=d24/2 #joint 2 to 3
+d34=d24/2 #joint 3 to 4
+d04=0.780 #joint 0 to 4
+d47=0.505 #joint 4 to 7
+d45=0.201 #joint 4 to 5
+d56=0.1985 #joint 5 to 6
+d46=d45+d56 #joint 4 to 6 #399.5 mm
+d67=0.1055 #joint 6 to 7
+d26=d24+d46
 tool=0.1 #10 cm calibration tool
 
 #---Homogenous Transformation Matrices with Joint 5 locked----
-A1=Matrix([[cos(theta1),0,-sin(theta1),0],[sin(theta1),0,cos(theta1),0],[0,-1,0,d1],[0,0,0,1]])
-A2=Matrix([[cos(theta2),0,sin(theta2),0],[sin(theta2),0,-cos(theta2),0],[0,1,0,0],[0,0,0,1]])
-A3=Matrix([[cos(theta3),0,-sin(theta3),0],[sin(theta3),0,cos(theta3),0],[0,-1,0,d3],[0,0,0,1]])
-A4=Matrix([[cos(theta4),0,sin(theta4),0],[sin(theta4),0,-cos(theta4),0],[0,1,0,0],[0,0,0,1]])
-A5=Matrix([[0,0,0,0],[0,0,0,0],[0,-1,0,d5],[0,0,0,1]])
-A6=Matrix([[cos(theta6),0,sin(theta6),0],[sin(theta6),0,-cos(theta6),0],[0,1,0,0],[0,0,0,1]])
-A7=Matrix([[cos(theta6),-sin(theta7),0,0],[sin(theta7), cos(theta7),0,0],[0,0,1,(d7+tool)],[0,0,0,1]])
+A1=Matrix([[cos(theta1),0,-sin(theta1),0],[sin(theta1),0,cos(theta1),0],[0,-1,0,d01],[0,0,0,1]])
+A2=Matrix([[cos(theta2),0,sin(theta2),0],[sin(theta2),0,-cos(theta2),0],[0,1,0,d12],[0,0,0,1]])
+A3=Matrix([[cos(theta3),0,-sin(theta3),0],[sin(theta3),0,cos(theta3),0],[0,-1,0,d23],[0,0,0,1]])
+A4=Matrix([[cos(theta4),0,sin(theta4),0],[sin(theta4),0,-cos(theta4),0],[0,1,0,d34],[0,0,0,1]])
+A5=Matrix([[cos(theta5),0,-sin(theta5),0],[sin(theta5),0,cos(theta5),0],[0,-1,0,d45],[0,0,0,1]])
+A6=Matrix([[cos(theta6),0,sin(theta6),0],[sin(theta6),0,-cos(theta6),0],[0,1,0,d56],[0,0,0,1]])
+A7=Matrix([[cos(theta6),-sin(theta7),0,0],[sin(theta7), cos(theta7),0,0],[0,0,1,(d67)],[0,0,0,1]])
+Atool=Matrix([[0,0,0,0],[0,0,0,0],[0,0,0,tool],[0,0,0,1]])
 
 #---Final Transformation np.array (Base to Frame 7)---
 H=A1*A2*A3*A4*A5*A6*A7
 
+#---Final Transformation np.array (Base to Tool tip)---
+H_tool=A1*A2*A3*A4*A5*A6*A7*Atool
 # print(H.shape) #4x4
 # print("\nForward Kinematics Transformation is: \n",np.array(H.evalf(2)))
 
 #X is first 3 elements of 4th column (d)
 X = Matrix( H[[0,1,2],:][:,3] ) #d_x, d_y, and d_z
-d_x=H[0,3]
-d_y=H[1,3]
-d_z=H[2,3]
+# d_x=H[0,3]
+# d_y=H[1,3]
+# d_z=H[2,3]
 # print(X)
 
 #Solve for joint angles based on EE position; Inv Kin.
-P_elbowat3=Matrix([[0],[0],[0],[1]])
-P_elbowto0=A1*A2*A3*P_elbowat3
-P_x3=P_elbowto0[0]
-P_y3=P_elbowto0[1]
-P_z3=P_elbowto0[2]
-t1=atan2(P_y3,P_x3)
-t2=acos((P_z3-d1)/d3)
-
-P_wristat5=Matrix([[0],[0],[0],[1]])
-P_elbowto0for5=A3*A4*A5*P_wristat5
-P_x5=P_elbowto0for5[0]
-P_y5=P_elbowto0for5[1]
-P_z5=P_elbowto0for5[2]
-t3=atan2(P_y5,P_x5)
-t4=acos((P_z5-d3)/d5)
-
-P_targetat7=Matrix([[0],[0],[0],[1]])
-P_elbowto0for7=A5*A6*A7*P_targetat7
-P_x7=P_elbowto0for5[0]
-P_y7=P_elbowto0for5[1]
-P_z7=P_elbowto0for5[2]
-t5=atan2(P_y7,P_x7)
-t6=acos((P_z7-d5)/(d7+tool))
-t7=0 #no need to rotate joint 7
-
-#solve for X with known EE position
-result=nsolve((t1,t2,t3,t4,t6,t7),(theta1,theta2,theta3,theta4,theta6,theta7),(0,0,0,0,0,0))
-# print(result)
-
-
-q_0 = Matrix([[theta1],[theta2],[theta4],[theta5],[theta6],[theta7]])
-# print("\n Symbolic q_0 is ",q_0)
-
-#--------PART 2: Inverse Velocity Kinematics---------------------------#
-
-#---Symbolic Jacobian-----
-jacob=X.jacobian(q_0)
-
-# print("\n\nParametric Jacobian is ", np.array(jacob.evalf(2)))
-# print(jacob.shape) #6x6 matrix
-
-#------------PART 3: Plot the circle---------------------------#
-
-#--Make Jacobian Numeric vice Parametric--
-J = jacob.subs([(theta1,t1),(theta2,t2), (theta4,t4), (theta3,t3),(theta6,t6),(theta7,t7)])
-# print ("\nNumeric Jacobian is: ", np.array(J.evalf(4)))
-# print("\nShape after subs: ", jacob.shape) #6x6 matrix
-
-#--Initial Joint Angles inputed into q_0---
-q_0 = q_0.subs([(theta1,t1),(theta2,t2), (theta4,t4), (theta3,t3),(theta6,t6),(theta7,t7)])
-q_previous = q_0;
-# print("\nInitial Joint Angles (q_0): ", np.array(q_0.evalf(4) ) )
-
+# atan2
+# acos
 goal_1=[-0.2, 0.7, 0.0]
 goal_2=[0.2, 0.7, 0.0]
 goal_3=[0.2, 0.4, 0.0]
@@ -124,7 +85,21 @@ d_x=goal_1[0]
 d_y=goal_1[1]
 d_z=goal_1[2]
 
-#Plot EE Position
+t1=acos(d_x/d46)
+b=tool+d67-d02
+d=sqrt(d_y*d_y+b*b)
+alpha=acos((d24*d24+d*d-d46*d46)/(2*d24*d))
+beta2=acos((d46*d46+d*d-d24*d24)/(2*d46*d))
+gamma2=acos((d24*d24+d46*d46-d*d)/(2*d24*d46))
+phi=atan2(b,d_y)
+t2=-(np.pi/2-(alpha+phi))
+t3=0 #lock out joint 3
+t4=-(np.pi-beta2)
+t5=0 #lock out joint 5
+t6=gamma2-phi-(np.pi/2)
+t7=0 #no need to rotate joint 7; angle is irrelevant to completion of task
+
+#-----Plot EE Position--------
 plt.figure(800)
 plt.axis([-0.5, 0.5, 0.3, 0.8])
 plt.title("ENPM 662 Project#2\nJerry Pittman (117707120)\nMaitreya Kulkarni (UMDID)")
@@ -201,6 +176,7 @@ pub_J1 = rospy.Publisher('/proj_2/PositionJointInterface_J1_controller/command',
 pub_J2 = rospy.Publisher('/proj_2/PositionJointInterface_J2_controller/command', Float64, queue_size=10) 
 pub_J3 = rospy.Publisher('/proj_2/PositionJointInterface_J3_controller/command', Float64, queue_size=10) 
 pub_J4 = rospy.Publisher('/proj_2/PositionJointInterface_J4_controller/command', Float64, queue_size=10) 
+pub_J5 = rospy.Publisher('/proj_2/PositionJointInterface_J5_controller/command', Float64, queue_size=10) 
 pub_J6 = rospy.Publisher('/proj_2/PositionJointInterface_J6_controller/command', Float64, queue_size=10) 
 pub_J7 = rospy.Publisher('/proj_2/PositionJointInterface_J7_controller/command', Float64, queue_size=10) 
     
@@ -233,7 +209,7 @@ def kuka_calibrate():
         elif d_x!=goal_1[0] and d_y!=goal_2[1]-(i*cal):#calibrating between goal 2-(i*cal) and  goal 1-i*cal
             if d_x==goal_1[0] and d_y==goal_1[1]-(i*cal):
                 d_y=d_y-cal #move down a row (-y)
-                i+=1
+                i=i+1
                 
             d_x=d_x-cal #move towards left (-x)
             
@@ -241,15 +217,12 @@ def kuka_calibrate():
         plt.figure(800)
         plt.scatter(d_x, d_y)
     
-        #Re-solve for joint angles based on new EE position
-        t1=
-        t2=
-        t3=
-        t4=
-        t6=
-        t7=
+        #Joint angles Should update automatically based on
+        # change in d_x and d_y
 
+        #Get Time from Ros
         seconds = rospy.get_time()
+        
         #plot Joint Angles
         plt.figure(1) #Joint 1
         plt.scatter(seconds, t1)
@@ -264,13 +237,14 @@ def kuka_calibrate():
         plt.figure(7)  #Joint 7
         plt.scatter(seconds, t7)
 
-        #publish joint angle
-        pub_J1.publish(t1) # publish the joint angle command
-        pub_J2.publish(t2) # publish the joint angle command
-        pub_J3.publish(t3) # publish the joint angle command
-        pub_J4.publish(t4) # publish the joint angle command
-        pub_J6.publish(t6) # publish the joint angle command
-        pub_J7.publish(t7) # publish the joint angle command
+        #publish joint angle commands
+        pub_J1.publish(t1)
+        pub_J2.publish(t2) 
+        pub_J3.publish(t3) 
+        pub_J4.publish(t4) 
+        pub_J5.publish(t5)
+        pub_J6.publish(t6) 
+        pub_J7.publish(t7) 
         
         if (d_x==goal_4[0] and d_y==goal_4[1]) or (d_x==goal_3[0] and d_y==goal_3[1]):
             print("Calibration Complete!")
@@ -284,11 +258,11 @@ if __name__=="__main__":
         pass
 
     finally:
-        #publish joint angle
-        pub_J1.publish(t1) # publish the joint angle command
-        pub_J2.publish(t2) # publish the joint angle command
-        pub_J3.publish(t3) # publish the joint angle command
-        pub_J4.publish(t4) # publish the joint angle command
-        pub_J6.publish(t6) # publish the joint angle comand
-        pub_J7.publish(t7) # publish the joint angle command
-
+        #publish joint angle commands
+        pub_J1.publish(t1) 
+        pub_J2.publish(t2)
+        pub_J3.publish(t3) 
+        pub_J4.publish(t4) 
+        pub_J5.publish(t5) 
+        pub_J6.publish(t6) 
+        pub_J7.publish(t7) 
